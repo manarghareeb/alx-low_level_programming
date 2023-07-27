8a9ce74d3557723d313feacf743d6441f1574bd3
manarghareeb <manarghareeb1973@gmail.com>
2023-07-27 15:56:55 -0700

    6-cap

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_number - print numbers chars
+ * @n: intager params
+ * Return: 0
+ */
+
+void print_number(int n)
+{
+	unsigned int n1;
+	
+	n1 = n;
+	if (n < 0)
+	{
+		_putchar('-');
+		n1 = -n;
+	}
+	if (n1 / 10 != 0)
+	{
+		print_number();
+	}
+	_putchar((n % 10) + '0');
+}
