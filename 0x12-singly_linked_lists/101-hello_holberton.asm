bb5ce8406882ff748ee9d0eabf53a31f4b14564b
manarghareeb <manarghareeb1973@gmail.com>
2023-08-24 17:11:49 -0700

    hello

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,9 @@
 section .text
-extern printf
-global main
+        extern  printf
+        global  main
 main:
-mov edi,msg
-mov eax, 0
-call printf
-section .data
-msg db 'Hello, Holbertion', 0xa, 0
+        mov             edi, msg
+        mov             eax, 0
+        call    printf
+section         .data
+        msg db 'Hello, Holbertion', 0xa, 0
